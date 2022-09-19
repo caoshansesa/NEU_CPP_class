6b3c82a4a176649ee51cd3d4a61c16d907db1532
Shan CAO <shan.cao@se.com>
2022-09-18 21:52:16 -0400

    Update the source code

--- prac_2/practise2.cpp
+++ prac_2/practise2.cpp
@@ -1,49 +1,61 @@
 #include <iostream>
+#include <memory>
 #include <string>
 
 using namespace std;
 
-int main()
-{
-	cout << "Guest Book\n\n";
+int main() {
+  cout << "Guest Book\n\n";
 
-	string first_name;
-	cout << "First name:\t";
-	cin >> first_name;             // get first string only
+  string first_name;
+  cout << "First name:\t";
+  cin >> first_name;  // get first string only
 
-	char middle_initial;
-	cout << "Middle initial:\t";
-	cin.ignore(100, '\n');         // ignore leftover chars and newline
-	middle_initial = cin.get();    // get first char only
+  string middle_initial;
+  cout << "Middle initial:\t";
+  cin.ignore(100, '\n');       // ignore leftover chars and newline
+  middle_initial = cin.get();  // get first char only
 
-	string last_name;
-	cout << "Last name:\t";
-	cin.ignore(100, '\n');         // ignore leftover chars and newline
-	getline(cin, last_name);       // get entire line
+  string last_name;
+  cout << "Last name:\t";
+  cin >> last_name;
 
-	string address;
-	cout << "Address:\t";
-	cin.ignore(100, '\n');         // ignore leftover chars and newline
+  string address;
+  cout << "Address:\t";
+  cin.ignore();
+  getline(cin, address);
 
-	string city;
-	cout << "City:\t";
-	getline(cin, city);            // get entire line
-	
-    string state_name;
-	cout << "state:\t";
-	cin.ignore(100, '\n');         // ignore leftover chars and newline
-	getline(cin, state_name);       // get entire line
-    
-	string zip_code;
-	cout << "zip_code\t";
-	getline(cin, zip_code);            // get entire line
+  string city;
+  cout << "City:\t";
+  cin >> city;
 
-	string country;
-	cout << "Country:\t";
-	getline(cin, country);         // get entire line
+  char state_name[2];
+  cout << "state:\t";
+  cin.getline(state_name,2);
 
-	cout << "\nENTRY\n"            // display the entry
-		<< first_name + ' ' + middle_initial + ". " + last_name + '\n'
-        << address + ' ' + city + ' ' + state_name + '\n' 
-		<< zip_code + ", " + country + "\n";
+  string zip_code;
+  cout << "zip_code\t";
+  cin >> zip_code;
+
+  string country;
+  cout << "Country:\t";
+  cin >> country;
+
+  cout << "\nENTRY\n"  // display the entry
+       << "First Name:\t" << first_name + "\n"
+       << "Middle Name:\t" << ' ' +  middle_initial + ".\n"
+       << "Last Name:\t" << last_name + "\n"
+       << "Address: \t" << address + "\n"
+       << "City: \t" << city + "\n"
+       << "State: \t" << state_name[0] + ' ' + state_name[1] + ' ' + " \n"
+       << "Country \t" << country + "\n";
+
+  cout << "\nENTRY\n"
+        << first_name + " " + middle_initial + ". " + last_name + "\n"
+        << address + "\n"
+        << city + ", " + state_name + "  " + zip_code + "\n"
+        << country + "\n";
 }
+
+
+
