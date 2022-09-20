f1a8cddbdf978ab822af7dbbdbc87e4ba08bef0c
shan cao <shancao@shans-iMac.fios-router.home>
2022-09-19 20:57:44 -0400

    update the code

--- prac_2/practise2.cpp
+++ prac_2/practise2.cpp
@@ -26,31 +26,32 @@ int main() {
   getline(cin, address);
 
   string city;
-  cout << "City:\t";
+  cout << "City:    \t";
   cin >> city;
 
   string state_name;
-  cout << "state:\t";
-  cin.ignore(); /* Flush the cin*/
+  cout << "state:   \t";
+  cin.ignore(); /* Flush the cin */
   getline(cin, state_name, '\n');
-  state_name = state_name.substr(0,2);/* get only the first two digits for the State name string */
+  /* get only the first two digits for the State name string */
+  state_name = state_name.substr(0, 2);
 
   string zip_code;
-  cout << "zip_code\t";
+  cout << "zip_code  \t";
   cin >> zip_code;
 
   string country;
-  cout << "Country:\t";
+  cout << "Country:  \t";
   cin >> country;
 
   cout << "\nENTRY\n"  // display the entry
-       << "First Name:\t" << first_name + "\n"
-       << "Middle Name:\t" << ' ' + middle_initial + ".\n"
-       << "Last Name:\t" << last_name + "\n"
-       << "Address: \t" << address + "\n"
-       << "City: \t" << city + "\n"
-       << "State: \t" << ' ' + state_name[0] + state_name[1] + ' ' + " \n"
-       << "Country \t" << country + "\n";
+       << "First Name:  \t" << first_name + "\n"
+       << "Middle Name: \t" << ' ' + middle_initial + ".\n"
+       << "Last Name:   \t" << last_name + "\n"
+       << "Address:     \t" << address + "\n"
+       << "City:        \t" << city + "\n"
+       << "State:       \t" << state_name + " \n"
+       << "Country      \t" << country + "\n";
 
   cout << "\nENTRY\n"
        << first_name + " " + middle_initial + ". " + last_name + "\n"
