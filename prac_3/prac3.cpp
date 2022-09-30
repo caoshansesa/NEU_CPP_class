3b99d50de06638f7a204ddd915aa6a8e006b8d41
Shan CAO <shan.cao@se.com>
2022-09-29 22:51:44 -0400

    update the seperator of str

--- prac_3/prac3.cpp
+++ prac_3/prac3.cpp
@@ -1,44 +1,84 @@
-#include <iostream>
 #include <fstream>
+#include <functional>
+#include <iostream>
+#include <iterator>
+#include <string>
 #include <vector>
+
 using namespace std;
 
-char performOperation(int x, int y, char op) {
-  int result;
-  switch (op) {
-    case '+' :
-      result = x + y;
-      break;
-    case '*' :
-      result = x* y;
-      break;
-    case '-' :
-      result = x - y;
-      break;
-    case '/' :
-      result = x / y;
-      break;
-    default:
-      result = 'F'; // for failed
+string performOperation(int x, int y, string op) {
+  double result = 0;
+  if (!op.compare("*")) {
+    result = x * y;
+  } else if (!op.compare("-")) {
+    result = x - y;
+  } else if (!op.compare("+")) {
+    result = x + y;
+  } else if (!op.compare("/")) {
+    result = x / y;
+  } else if (!op.compare("%")) {
+    result = x % y;
+  } else if (!op.compare("%")) {
   }
-  return (char) result;
+  // TODO: if the result is a minus number, it will be a bug
+  return to_string(result);
 }
 
+vector<string> seperate_strings(string line) {
+  int coma_counter(0);
+  vector<string> str_group;
+  string a, b, op;
+  for (char cc : line) {
+    if ((cc != ',') && (coma_counter == 0)) {
+      a.push_back(cc);
+    } else {
+      coma_counter++;
+    }
+
+    if ((cc != ',') && (coma_counter == 1)) {
+      b.push_back(cc);
+    } else {
+      coma_counter++;
+    }
+
+    if ((cc != ',') && (coma_counter == 2)) {
+      op.push_back(cc);
+    } else {
+      coma_counter++;
+    }
+
+    if (coma_counter == 3) {
+      str_group.push_back(a);
+      str_group.push_back(b);
+      str_group.push_back(op);
+    }
+  }
+  return str_group;
+}
 
 int main() {
-  ifstream infile("test.txt");
+  ifstream infile("text.txt");
   ofstream outfile("output.txt");
   string line;
-  int x, y;
-  char op;
- while (getline(infile, line)) {
-    x = (int) line[0] - 48;
-    y = (int) line[2] - 48;
-    op = line[4];
-    char result = performOperation(x, y, op);
-    cout << result;
-    outfile << line << ',' << result <<endl;
+  signed int x, y;
+  string oprt;
+  while (getline(infile, line)) {
+    x = (int)line[0] - 48;
+    y = (int)line[2] - 48;
+
+    // Clear the operator sting
+    oprt.clear();
+    // determine whther this a two digit operat or not
+    if (line[5] != '\0') {
+      oprt.push_back(line[4]);
+      oprt.push_back(line[5]);
+    } else {
+      oprt.push_back(line[4]);
+    }
+    string result = performOperation(x, y, oprt);
+    outfile << line << ',' << result << endl;
   }
- 
+
   return 0;
 }
