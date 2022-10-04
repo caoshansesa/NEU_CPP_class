7d61e4cc013eb1bcc86aaa522fcbe7505ec0a242
shan cao <cao.shan2@northeastern.edu>
2022-10-04 00:19:44 -0400

    Update the entire code structure

--- hw2/src/hw2.cpp
+++ hw2/src/hw2.cpp
@@ -1,7 +1,4 @@
-#include <cstdlib>
-#include <iostream>
-
-using namespace std;
+#include "../inc/hw2.hpp"
 
 int GetInput() {
   int choice;
@@ -9,29 +6,69 @@ int GetInput() {
   return choice;
 }
 
-void DisplayMainMenu() {
+void ShowMainMenu() {
   cout << "Main Menu\n";
-  cout << "Please make your selection\n";
-  cout << "1 - Start game\n";
-  cout << "2 - Options\n";
-  cout << "3 - Quit\n";
-  cout << "Selection: ";
+  cout << "1 - Display the seating map/chart showing all seats.\n";
+  cout << "2 - Print a passenger manifest. \n";
+  cout << "3 - Load the saved seat. \n";
+  cout << "4 - Save the current seat. \n";
+  cout << "5 - Choose a Seat. \n";
+  cout << "6 - Cancel Seat Assignment. \n";
+  cout << "7 - Print Your Boarding PASS \n";
+  cout << "8 - Quit the system \n";
 }
 
-void DisplayOptionsMenu() {
-  cout << "Options Menu\n";
-  cout << "Please make your selection\n";
-  cout << "1 - Difficulty\n";
-  cout << "2 - Sound\n";
-  cout << "3 - Back\n";
-  cout << "Selection: ";
-}
-
-void Options() {
+void option_for_main_menu() {
   int choice = 0;
   do {
     system("clear");
-    DisplayOptionsMenu();
+    ShowMainMenu();
+    choice = GetInput();
+    switch (choice) {
+      case 1:
+        cout << "difficulty stuff";
+        break;
+      case 2:
+        cout << "difficulty stuff";
+        break;
+      case 3:
+        cout << "difficulty stuff";
+        break;
+      case 4:
+        cout << "difficulty stuff";
+        break;
+      case 5:
+        cout << "difficulty stuff";
+        break;
+      case 6:
+        cout << "difficulty stuff";
+        break;
+      case 7:
+        cout << "difficulty stuff";
+        break;
+      case 8:
+        cout << "quite The program \n";
+        break;
+      default:
+        break;
+    }
+  } while (choice != 8);
+}
+
+void print_2_passager_manifest() {}
+
+void print_2_seat_map() {}
+
+void Display_2_Print_passager_manifest() {
+  cout << " 1 - Print Passager Manifest" << endl;
+  cout << " 2 - Print seat Map" << endl;
+}
+
+void option_for_2_print_passager_manifest() {
+  int choice = 0;
+  do {
+    system("clear");
+    Display_2_Print_passager_manifest();
     choice = GetInput();
     switch (choice) {
       case 1:
@@ -48,19 +85,69 @@ void Options() {
   } while (choice != 3);
 }
 
+void load_3_passager_manifest() {
+  // Show Blank and show the main screen
+}
+
+void save_4_passager_manifest() {
+  // Show Blank and show the main screen
+}
+
+void print_menu_5()
+{
+
+}
+
+void option_for_menu_5()
+{
+    
+}
+
+void choose_seats_5() {
+   // Type define 1, First class, Bu..clas? ecnoy class?
+   /*
+        * Which_class_the_user_input()
+    {
+        FirstClass()
+        BusinessClass()
+        Economy()
+    }
+   */
+   //
+   // Assign a random seats 
+   //   RandomNumGenerator()
+//	    int = num_generator()
+//	            if(int not seen before)                             
+  // Check_Whether_this_seat_is_available()
+  // Downgrade_compatiple test
+  //
+}
+
+
+void Option_Cancel_seat_assignment_6()
+{
+   
+    //print current seat alignment?
+    // save the seat info?
+}
+
+
+void print_boarding_pass_7(int seatNumber, string name){}
+
+
+
+
 int main(int argc, char *argv[]) {
   int choice = 0;
-
   do {
     system("clear");
-    DisplayMainMenu();
+    ShowMainMenu();
     choice = GetInput();
     switch (choice) {
       case 1:
         cout << "Pew pew!\n";
         break;
       case 2:
-        Options();
         break;
       case 3:
         cout << "Goodbye!";
