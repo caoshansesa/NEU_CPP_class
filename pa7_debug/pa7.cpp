1fb9d2ef0d7e750b916e4480fb4ce83792430860
Shan CAO <shan.cao@se.com>
2022-10-20 18:38:24 -0400

    commit the code

--- pa7_debug/pa7.cpp
+++ pa7_debug/pa7.cpp
@@ -1,39 +1,33 @@
 #include <stdio.h>
 
+#include <cstdio>
+
 #define MAX_LETTERS 26
 
 char alphabet[MAX_LETTERS];
 
-void initialize_alphabet(char *a) {
-  char *p;
+void initialize_alphabet(char *input_array) {
   char current_letter = 'a';
-  for (p=a;p<(a+MAX_LETTERS);p++);{
-    *p = current_letter++;
+  for (int i = 0; i < MAX_LETTERS; i++) {
+    input_array[i] = current_letter;
+    current_letter++;
   }
-}  
+}
 
-void reverse_print_alpha(char *b) {
-  static int var = 0;
-  char *p;
-  
-  var?(p=NULL):(b=NULL);
-  var *= 1;
+void reverse_print_alpha(char *input_array) {
   printf("\nReverse alpha...\n");
-  initialize_alphabet(b);
-  for (p=(b+MAX_LETTERS-1);p>=b;p--);{
-    printf("%c ",*b);
+  initialize_alphabet(input_array);
+  for (int i = MAX_LETTERS +1; i > 0; --i) {
+    printf("%c ", input_array[i-1]);
   }
+
   printf("\n");
-  return ;
+  return;
 }
 
 int main(void) {
-  int i;
+  int i = 0;
   while (i++ < 5) {
     reverse_print_alpha(alphabet);
   }
 }
-
-  
-
-  
