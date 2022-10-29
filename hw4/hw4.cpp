85e448fd24e54ecb0d40cfd4cd5152e0de08e0f6
shan cao <cao.shan2@northeastern.edu>
2022-10-29 19:25:17 -0400

    Add the student info struct, Add the map container

---
+++ hw4/hw4.cpp
@@ -0,0 +1,9 @@
+#include "hw4.hpp"
+
+int main() {
+  fstream fout;
+  fout.open("Assignment4_Pref1.csv", ios::in);
+
+
+  return 0;
+}
---
+++ hw4/hw4.hpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <map>
+#include <vector>
+#include <stdio.h>
+#include <fstream>
+
+using namespace std;
+struct student_info
+{
+		int c_level;
+		int gdb_level;
+		int Alg_level;
+		vector<string> name_Devil;
+		vector<string> name_Angel;
+		int Grounp_num_been_asign;
+};
+map<string, struct student_info> _student_info;
