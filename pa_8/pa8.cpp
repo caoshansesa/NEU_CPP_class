00cada116ff5a71d329d687bb92d92558bd5e58c
Shan CAO <shan.cao@se.com>
2022-10-31 14:29:50 -0400

    Update the orders

--- pa_8/pa8.cpp
+++ pa_8/pa8.cpp
@@ -12,8 +12,8 @@
 using namespace std;
 
 void print(pair<string, vector<int> > _student_score) {
-  cout << _student_score.first;
-  for (int i = 0; i < _student_score.second.size(); i += 1) {
+  cout << _student_score.first << ": ";
+  for (int i = 0; i < 7; i += 1) {
     cout << " " << _student_score.second.back() << "  ";
     _student_score.second.pop_back();
   }
@@ -23,7 +23,7 @@ void print(pair<string, vector<int> > _student_score) {
 int main() {
   map<string, vector<int> > studnent_grade;
   vector<int> student_score[7];
-  string name_student[5] = {"Shan", "Amel", "Ted", "Moh", "Jeff"};
+  string name_student[5] = {"Shan", "Amel", "Ted ", "Moh ", "Jeff"};
 
   for (int j = 0; j < 5; j++) {
     for (int i = 0; i < 7; i++) {
@@ -33,7 +33,7 @@ int main() {
         pair<string, vector<int> >(name_student[j], student_score[j]));
   }
 
-  cout << "student score" << endl;
+  cout << "Name  HW1  HW2  HW3  HW4  HW5  HW6  HW7  " << endl;
 
   for_each(studnent_grade.begin(), studnent_grade.end(), print);
   return 0;
