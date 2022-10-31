6d5254420c92f6c16edc2e8cf5fbeeda97102376
Shan CAO <shan.cao@se.com>
2022-10-31 15:45:21 -0400

    Add the average

--- pa_8/pa8.cpp
+++ pa_8/pa8.cpp
@@ -10,14 +10,30 @@
 #include <vector>
 
 using namespace std;
+int index_vector = 0;
 
-void print(pair<string, vector<int> > _student_score) {
+void print_list_info(pair<string, vector<int> > _student_score) {
   cout << _student_score.first << ": ";
   for (int i = 0; i < 7; i += 1) {
     cout << " " << _student_score.second.back() << "  ";
     _student_score.second.pop_back();
   }
   cout << endl;
+}
+
+double average[7] = {0};
+double sum[7] = {0};
+void print_average(pair<string, vector<int> > _student_score) {
+  index_vector++;
+  for (int i = 0; i < 7; i += 1) {
+    sum[i] += _student_score.second.at(i);
+  }
+  if (index_vector == 5) {
+    for (int i = 0; i < 7; i += 1) {
+      average[i] = sum[i] / 5;
+      cout << "Assignment " << i + 1 << " average is :" << average[i] << endl;
+    }
+  }
 }
 
 int main() {
@@ -35,6 +51,10 @@ int main() {
 
   cout << "Name  HW1  HW2  HW3  HW4  HW5  HW6  HW7  " << endl;
 
-  for_each(studnent_grade.begin(), studnent_grade.end(), print);
+  for_each(studnent_grade.begin(), studnent_grade.end(), print_list_info);
+
+  cout << "The average " << endl;
+  for_each(studnent_grade.begin(), studnent_grade.end(), print_average);
+
   return 0;
 }
