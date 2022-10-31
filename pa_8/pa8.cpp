be41156c0bdeb2d34a747fb8fa764f9c80dcc567
Shan CAO <shan.cao@se.com>
2022-10-31 11:37:28 -0400

    Add function object expression

--- pa_8/pa8.cpp
+++ pa_8/pa8.cpp
@@ -1,16 +1,40 @@
-#include <cstdlib>
 #include <stdio.h>
+
+#include <algorithm>
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
 #include <map>
+#include <string>
+#include <utility>
 #include <vector>
 
 using namespace std;
 
-int main()
-{
-    map< string, vector<int> > studnent_grade;
-    vector<int> student_score;
-    for (int i = 0; i<5; i++) {
-        student_score.push_back(rand()%50 + 10);
+void print(pair<string, vector<int> > _student_score) {
+  cout << _student_score.first;
+  for (int i = 0; i < _student_score.second.size(); i += 1) {
+    cout << " " << _student_score.second.back() << "  ";
+    _student_score.second.pop_back();
+  }
+  cout << endl;
+}
+
+int main() {
+  map<string, vector<int> > studnent_grade;
+  vector<int> student_score[7];
+  string name_student[5] = {"Shan", "Amel", "Ted", "Moh", "Jeff"};
+
+  for (int j = 0; j < 5; j++) {
+    for (int i = 0; i < 7; i++) {
+      student_score[j].push_back(rand() % 50 + 10);
     }
-    return 0;
+    studnent_grade.insert(
+        pair<string, vector<int> >(name_student[j], student_score[j]));
+  }
+
+  cout << "student score" << endl;
+
+  for_each(studnent_grade.begin(), studnent_grade.end(), print);
+  return 0;
 }
