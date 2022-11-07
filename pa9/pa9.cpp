ced4ae609e3105e4b040f5bc858d2105b06c8750
shan cao <cao.shan2@northeastern.edu>
2022-11-06 23:11:50 -0500

    update

--- pa9/pa9.cpp
+++ pa9/pa9.cpp
@@ -1,32 +1,9 @@
-/*
- * =====================================================================================
- *
- *       Filename:  pa9.cpp
- *
- *    Description:
- *
- *        Version:  1.0
- *        Created:  11/06/2022 08:32:11
- *       Revision:  none
- *       Compiler:  gcc
- *
- *         Author:  YOUR NAME (),
- *   Organization:
- *
- * =====================================================================================
- */
 #include <stdlib.h>
 
 #include <list>
 #include <string>
 
 using namespace std;
-class GROUP {
- private:
-  string group_name;
-  string course_name;
-  list<string> student_in_group;
-};
 
 class Student {
  private:
@@ -35,12 +12,32 @@ class Student {
   int course_number;
 };
 
+class GROUP {
+ private:
+  string group_name;
+  string course_name;
+  list<Student> student_in_group;
+};
+
 class Course {
  private:
   int unique_course_number;
   string course_name;
   int course_number;
-  list<string> student_Enroll_in_course;
+  list<Student> student_Enroll_in_course;
+
+ public:
+  void operator<<(const Student& student_list);
+
+  list<GROUP>& operator/(const int& NumOfGroups);
 };
 
-int main() {}
+list<GROUP>& Course::operator/(const int& NumOfGroups) {
+
+}
+
+void Course::operator<<(const Student& student_list) {}
+
+int main() {
+    
+}
