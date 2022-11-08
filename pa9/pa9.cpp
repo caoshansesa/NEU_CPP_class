18162260a9ec431315068c8b3aea715fa2b4eb74
shan cao <cao.shan2@northeastern.edu>
2022-11-07 19:59:16 -0500

    add update

--- pa9/pa9.cpp
+++ pa9/pa9.cpp
@@ -27,17 +27,21 @@ class Course {
   list<Student> student_Enroll_in_course;
 
  public:
+  Course(string _course_name, int _course_number, int _unique_course_number) {
+    _course_name = course_name;
+    _course_number = course_number;
+    _unique_course_number = unique_course_number;
+  };
   void operator<<(const Student& student_list);
 
   list<GROUP>& operator/(const int& NumOfGroups);
 };
 
-list<GROUP>& Course::operator/(const int& NumOfGroups) {
-
-}
+list<GROUP>& Course::operator/(const int& NumOfGroups) {}
 
 void Course::operator<<(const Student& student_list) {}
 
 int main() {
-    
+  Course cs3520("cs3520", 3520, 111);
+
 }
