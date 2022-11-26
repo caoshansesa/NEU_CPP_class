e7b810c1b45e9e2a36591891e2e77528cfad89c1
shan cao <cao.shan2@northeastern.edu>
2022-11-26 15:30:47 -0500

    c format

--- pa11_debug/DebugT-2.cpp
+++ pa11_debug/DebugT-2.cpp
@@ -1,64 +1,66 @@
 // Debug T-2
-// Function template finds biggest of two or three objects. The function works on any data type which has greater than operator defined.
+// Function template finds biggest of two or three objects. The function works on any data type
+// which has greater than operator defined.
 
 #include <iostream>
 #include <string>
 using namespace std;
-template <class T>
-void findBiggest(T a, int b, T c)
+
+template<class T> void findBiggest(T a, int b, T c)
 {
-   T large = a;
-   if (b > large)
-      large = b;
-   if (c < large)
-      large = c;
-   cout << "The largest of " << a << " " << b << " " << c << " is " << a << endl;
+    T large = a;
+    if (b > large) large = b;
+    if (c < large) large = c;
+    cout << "The largest of " << a << " " << b << " " << c << " is " << a << endl;
 }
-template <class T>
-void findBiggest(T a, T b)
+
+template<class T> void findBiggest(T a, T b)
 {
-   T large = a;
-   if (b == large)
-      large = b;
-   cout << "The larger of " << a << " " << b << " is " << larger << endl;
+    T large = a;
+    if (b == large) large = b;
+    cout << "The larger of " << a << " " << b << " is " << larger << endl;
 }
+
 class BankAccount
 {
-   friend ostream &operator<<(ostream &, const BankAccount &);
+    friend ostream& operator<<(ostream&, const BankAccount&);
 
 private:
-   string name;
-   int amount;
+    string name;
+    int    amount;
 
 public:
-   BankAccount(const int);
-   bool operator>(const BankAccount &);
+    BankAccount(const int);
+    bool operator>(const BankAccount&);
 };
-BankAccount::BankAccount(const string &name, const int amount)
+
+BankAccount::BankAccount(const string& name, const int amount)
 {
-   name = name;
-   amount = amount;
+    name   = name;
+    amount = amount;
 }
-ostream &operator<<(ostream &out, const BankAccount &bh)
+
+ostream& operator<<(ostream& out, const BankAccount& bh)
 {
-   out << "Name << " name << " Amount: " << amount << endl;
-   return out;
+    out << "Name << " name << " Amount: " << amount << endl;
+    return out;
 }
-bool BankAccount::operator>(const BankAccount &bh)
+
+bool BankAccount::operator>(const BankAccount& bh)
 {
-   bool flag == false;
-   if (amount > bh.amount)
-      flag = true;
-   return flag;
+    bool flag == false;
+    if (amount > bh.amount) flag = true;
+    return flag;
 }
+
 int main()
 {
-   BankAccount bAA("A", 800);
-   BankAccount bAB("B", 120);
-   BankAccount bAC("C", 500);
-   findBiggest(bAA, bAB);
-   findBiggest(bAB, bAC);
-   findBiggest(bAC, bAA);
-   findBiggest(bAA, bAB, bAC);
-   return 0;
+    BankAccount bAA("A", 800);
+    BankAccount bAB("B", 120);
+    BankAccount bAC("C", 500);
+    findBiggest(bAA, bAB);
+    findBiggest(bAB, bAC);
+    findBiggest(bAC, bAA);
+    findBiggest(bAA, bAB, bAC);
+    return 0;
 }
