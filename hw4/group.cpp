f35776fbc3dafe058a99b982c0146c4bd83e6cc6
Shan CAO <shan.cao@se.com>
2022-12-01 22:27:56 -0500

    run c-formater for code style

--- hw4/group.cpp
+++ hw4/group.cpp
@@ -16,161 +16,190 @@ int group_n = 0;
 
 int c = 10, gdb = 5, algo = 5;
 
-map<string, int> student_non_dev;  // every student with out a devil list and
-                                   // their weighted score
-map<string, int>
-    student_dev;  // student with  a devil list and their wighted score
+map<string, int> student_non_dev; // every student with out a devil list and
+                                  // their weighted score
+map<string, int> student_dev;     // student with  a devil list and their wighted score
 map<string, int> student_groups;  // every student and their team assigned
 
+map<string, int>::iterator finder;
 
-map<string, int> ::iterator finder;
-
-int w_sum(const student_info &s) {
-  return c * s.c_level + gdb * s.gdb_level + algo * s.algo_level;
+int w_sum(const student_info &s)
+{
+    return c * s.c_level + gdb * s.gdb_level + algo * s.algo_level;
 };
 
-void print_team_info(map<string, int> students_str) {
-  for (int i = 0; i < group_n; i++) {
-    int team_sum = 0;
-    auto it = students_str.begin();
-    cout << "This is group " << i << "\n";
-    while (it != students_str.end()) {
-      if (it->second == i) {
-        cout << it->first << " ";
-        if (student_non_dev.find(it->first) != student_non_dev.end()) {
-            team_sum += student_non_dev.find(it->first)->second;
-        }
-        else {
-            team_sum += student_dev.find(it->first)->second;
-        }
-      }
-      it++;
-    }
-    cout << "group_score is " << team_sum << endl;
-    cout << endl;
-  }
-}
-
-
-
-
-bool compare_v(const pair<string, int> &a, const pair<string, int> &b) {
-  return a.second < b.second;
-}
-
-int sort_m(map<string, int> &student_map) {
-  vector<pair<string, int> > vec;
-
-  for (const auto &p : student_map) {
-    vec.emplace_back(p);
-  }
-
-  sort(vec.begin(), vec.end(), compare_v);
-
-  return 0;
-}
-
-int group_students(Student_info_container students) {
-  // create a copy of students with devil list and without devil list
-  cout << " Please input your per_group student number: 3 or 4 \n";
-  int max_per_group = 0;
-  cin >> max_per_group;
-  for (auto p : students) {
-    auto key = p.first;
-    auto value = students[key];
-    if (value.devil.empty()) {
-      student_non_dev[key] = w_sum(value);  // add to non_dev map
-    } else {
-      student_dev[key] = w_sum(value);
-    }                  // add to dev map
-    total_students++;  // total number of students in container
-  }
-  group_n = total_students / max_per_group;
-  // sort maps
-  sort_m(student_non_dev);
-  sort_m(student_dev);
-
-  // assign every student__dev first
-  auto it = student_dev.begin();
-  int n = 0;
-  while (n < group_n && it != student_dev.end()) {
-    student_groups[it->first] = n;
-    n++;
-    it++;
-  }
-
-  int r = 0;
-  while (r < max_per_group) {
-    // assign the rest of students in student_dev
-    n = 0;
-    while (n < group_n && it != student_dev.end()) {
-      int curr_g = 0, prev_g = 0;
-      bool found_dev = false;
-      while (!found_dev) {
-        curr_g = rand() % group_n;
-        if (curr_g == prev_g) {
-          found_dev = true;
-        }
-        for (auto p : students) {
-          if (it->second == curr_g) {
-            auto itr2 = p.second.devil.begin();
-            int j = 0;
-            while (itr2 != p.second.devil.end()) {
-              if (it->first == p.second.devil.at(j)) {
-                found_dev = false;
-              }
-              itr2++;
-              j++;
+void print_team_info(map<string, int> students_str)
+{
+    for (int i = 0; i < group_n; i++)
+    {
+        int team_sum = 0;
+        auto it = students_str.begin();
+        cout << "This is group " << i << "\n";
+        while (it != students_str.end())
+        {
+            if (it->second == i)
+            {
+                cout << it->first << " ";
+                if (student_non_dev.find(it->first) != student_non_dev.end())
+                {
+                    team_sum += student_non_dev.find(it->first)->second;
+                }
+                else
+                {
+                    team_sum += student_dev.find(it->first)->second;
+                }
             }
-          }
+            it++;
         }
-      }
-
-      student_groups[it->first] = n;
-      prev_g = curr_g;
-      it++;
-      n++;
+        cout << "group_score is " << team_sum << endl;
+        cout << endl;
     }
-    r++;
-  }
+}
 
-  // assign those who don't have a dev list based on their pref before
-  // randomizing a number
+bool compare_v(const pair<string, int> &a, const pair<string, int> &b)
+{
+    return a.second < b.second;
+}
 
-  r = 0;
+int sort_m(map<string, int> &student_map)
+{
+    vector<pair<string, int> > vec;
 
-  it = student_non_dev.begin();
-  while (r < max_per_group) {
-    // assign the rest of students in student_dev
-    n = 0;
-    while (n < group_n && it != student_non_dev.end()) {
-      int curr_g = 0, prev_g = 0;
-      bool found_dev = false;
-      while (!found_dev) {
-        if (curr_g == prev_g) { found_dev = true; }
-        for (auto p : students) {
-          if (it->second == curr_g) {
-            auto itr2 = p.second.devil.begin();
-            int j = 0;
-            while (itr2 != p.second.devil.end()) {
-              if (it->first == p.second.devil.at(j)) {
-                found_dev = false;
-              }
-              itr2++;
-              j++;
+    for (const auto &p : student_map)
+    {
+        vec.emplace_back(p);
+    }
+
+    sort(vec.begin(), vec.end(), compare_v);
+
+    return 0;
+}
+
+int group_students(Student_info_container students)
+{
+    // create a copy of students with devil list and without devil list
+    cout << " Please input your per_group student number: 3 or 4 \n";
+    int max_per_group = 0;
+    cin >> max_per_group;
+    for (auto p : students)
+    {
+        auto key = p.first;
+        auto value = students[key];
+        if (value.devil.empty())
+        {
+            student_non_dev[key] = w_sum(value); // add to non_dev map
+        }
+        else
+        {
+            student_dev[key] = w_sum(value);
+        }                 // add to dev map
+        total_students++; // total number of students in container
+    }
+    group_n = total_students / max_per_group;
+    // sort maps
+    sort_m(student_non_dev);
+    sort_m(student_dev);
+
+    // assign every student__dev first
+    auto it = student_dev.begin();
+    int n = 0;
+    while (n < group_n && it != student_dev.end())
+    {
+        student_groups[it->first] = n;
+        n++;
+        it++;
+    }
+
+    int r = 0;
+    while (r < max_per_group)
+    {
+        // assign the rest of students in student_dev
+        n = 0;
+        while (n < group_n && it != student_dev.end())
+        {
+            int curr_g = 0, prev_g = 0;
+            bool found_dev = false;
+            while (!found_dev)
+            {
+                curr_g = rand() % group_n;
+                if (curr_g == prev_g)
+                {
+                    found_dev = true;
+                }
+                for (auto p : students)
+                {
+                    if (it->second == curr_g)
+                    {
+                        auto itr2 = p.second.devil.begin();
+                        int j = 0;
+                        while (itr2 != p.second.devil.end())
+                        {
+                            if (it->first == p.second.devil.at(j))
+                            {
+                                found_dev = false;
+                            }
+                            itr2++;
+                            j++;
+                        }
+                    }
+                }
             }
-          }
+
+            student_groups[it->first] = n;
+            prev_g = curr_g;
+            it++;
+            n++;
         }
-      }
-      student_groups[it->first] = n;
-      prev_g = curr_g;
-      it++;
-      n++;
+        r++;
     }
-    r++;
-  }
 
-  print_team_info(student_groups);
+    // assign those who don't have a dev list based on their pref before
+    // randomizing a number
 
-  return 0;
+    r = 0;
+
+    it = student_non_dev.begin();
+    while (r < max_per_group)
+    {
+        // assign the rest of students in student_dev
+        n = 0;
+        while (n < group_n && it != student_non_dev.end())
+        {
+            int curr_g = 0, prev_g = 0;
+            bool found_dev = false;
+            while (!found_dev)
+            {
+                if (curr_g == prev_g)
+                {
+                    found_dev = true;
+                }
+                for (auto p : students)
+                {
+                    if (it->second == curr_g)
+                    {
+                        auto itr2 = p.second.devil.begin();
+                        int j = 0;
+                        while (itr2 != p.second.devil.end())
+                        {
+                            if (it->first == p.second.devil.at(j))
+                            {
+                                found_dev = false;
+                            }
+                            itr2++;
+                            j++;
+                        }
+                    }
+                }
+            }
+            student_groups[it->first] = n;
+            prev_g = curr_g;
+            it++;
+            n++;
+        }
+        r++;
+    }
+
+    print_team_info(student_groups);
+
+    return 0;
 }
--- hw4/readfile.cpp
+++ hw4/readfile.cpp
@@ -1,20 +1,20 @@
 /*readfile.cpp*/
 
-#include <iostream>
-#include <vector>
-#include <map>
-#include <string>
 #include <algorithm>
 #include <cstdlib>
-#include <cstdlib>
 #include <fstream>
+#include <iostream>
+#include <map>
 #include <sstream>
+#include <string>
+#include <vector>
 
 #include "header.hpp"
 
 using namespace std;
 
-Student_info_container load_data_from_csv_into_file(const string& file_name) {
+Student_info_container load_data_from_csv_into_file(const string &file_name)
+{
     Student_info_container _student_info;
     fstream fin;
     string temp, line, word;
@@ -24,55 +24,58 @@ Student_info_container load_data_from_csv_into_file(const string& file_name) {
 
     fin.open(file_name, ios::in);
 
-    getline(fin, line);  // remove the first row
+    getline(fin, line); // remove the first row
     struct student_info _student_info_local;
 
-    while (getline(fin, line)) {
+    while (getline(fin, line))
+    {
         // break file into words
         stringstream s(line);
-        //remove \r from the end
-        getline(s, word, ',');  // username
+        // remove \r from the end
+        getline(s, word, ','); // username
         username = word;
 
-        getline(s, word, ',');  // c_level
+        getline(s, word, ','); // c_level
         _student_info_local.c_level = stoi(word);
 
-        getline(s, word, ',');  // gdb_level
+        getline(s, word, ','); // gdb_level
         _student_info_local.gdb_level = stoi(word);
 
-        getline(s, word, ',');  // Alg_level
+        getline(s, word, ','); // Alg_level
         _student_info_local.algo_level = stoi(word);
 
-        getline(s, word, ',');  // save all the name in one line string
+        getline(s, word, ','); // save all the name in one line string
         name_Angel_list = word;
 
-        getline(s, word, ',');  // save all the name in one line string
+        getline(s, word, ','); // save all the name in one line string
         name_Devil_list = word;
 
         _student_info_local.angel.clear();
         _student_info_local.devil.clear();
 
-        name_Devil_list.erase(name_Devil_list.size() -1);
+        name_Devil_list.erase(name_Devil_list.size() - 1);
 
-        if(!name_Devil_list.empty()) {
+        if (!name_Devil_list.empty())
+        {
 
             stringstream nam_D(name_Devil_list);
-            while (getline(nam_D, word, ' ')) {
+            while (getline(nam_D, word, ' '))
+            {
                 _student_info_local.devil.push_back(word);
             }
         }
 
-        if(!name_Angel_list.empty()) {
-            name_Angel_list.erase(name_Angel_list.size() -1);
+        if (!name_Angel_list.empty())
+        {
+            name_Angel_list.erase(name_Angel_list.size() - 1);
             stringstream nam_A(name_Angel_list);
-            while (getline(nam_A, word, ' ')) {
+            while (getline(nam_A, word, ' '))
+            {
                 _student_info_local.angel.push_back(word);
             }
         }
 
-        _student_info.insert(
-                pair<string, student_info>(username, _student_info_local));
-
+        _student_info.insert(pair<string, student_info>(username, _student_info_local));
     }
     return _student_info;
 }
