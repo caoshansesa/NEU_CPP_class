bca2cc5424b9c48618407b56f6faccb47b0f855a
Amel Elawad <aminelfadilelawad.a@northeastern.edu>
2022-12-11 19:47:25 -0500

    Update state_def.hpp

--- final_project/state_def.hpp
+++ final_project/state_def.hpp
@@ -6,55 +6,95 @@
 static int terminal_x, terminal_y = 0;
 using namespace std;
 
-class User
-{
-  public:
+class Task {
+public:
+    int id;
+    string type;
+    int priority;
+    string assignDate;
+    string dueDate;
+    string completionDate;
+    string title;
+    string assignees;
+    string status;
+    string issues;
+
+    Json::Value toJson() {
+        Json::Value task_json;
+        task_json["id"] = id;
+        task_json["type"] = type;
+        task_json["priority"] = priority;
+        task_json["assignDate"] = assignDate;
+        task_json["dueDate"] = dueDate;
+        task_json["completionDate"] = completionDate;
+        task_json["title"] = title;
+        task_json["status"] = status;
+        task_json["assignees"] = assignees;
+        task_json["issues"] = issues;
+
+        return task_json;
+
+    }
+};
+
+class User {
+public:
     string name;
     string userName;
     string jobTitle;
-    bool canDeleteTask = true;
-    bool canEditTask = true; // only certain attributes
-    bool canAddTask = true;
-    bool canDeletePrj = true;
-    bool canEditPrj = true;
-    bool canAddPrj = true;
+    bool canDeleteTask;
+    bool canEditTask;
+    bool canAddTask;
+    bool canDeletePrj;
+    bool canEditPrj;
+    bool canAddPrj;
+
+    Json::Value toJson() {
+        Json::Value user_json;
+        user_json["name"] = name;
+        user_json["userName"] = userName;
+        user_json["jobTitle"] = jobTitle;
+        user_json["canDeleteTask"] = canDeleteTask;
+        user_json["canEditTask"] = canEditTask;
+        user_json["canAddTask"] = canAddTask;
+        user_json["canDeletePrj"] = canDeletePrj;
+        user_json["canEditPrj"] = canEditPrj;
+        user_json["canAddPrj"] = canAddPrj;
+
+        return user_json;
+    }
 };
 
-class ProjectOwner : User
-{
-    ProjectOwner(){};
+class ProjectOwner : User {
+    ProjectOwner() {};
 };
 
-class Developer : User
-{
-    Developer()
-    {
+class Developer : User {
+    Developer() {
         canDeletePrj = false;
         canDeleteTask = false;
         canEditPrj = false;
         canAddPrj = false;
         canEditTask = true;
         canAddTask = true;
+
     };
 };
 
-class QAEngineers : User
-{
-    QAEngineers()
-    {
+class QAEngineers : User {
+    QAEngineers() {
         canDeletePrj = false;
         canDeleteTask = false;
         canEditPrj = false;
         canAddPrj = false;
         canEditTask = false;
         canAddTask = false;
+
     };
 };
 
-class ProjectManager : User
-{
-    ProjectManager()
-    {
+class ProjectManager : User {
+    ProjectManager() {
         canDeletePrj = false;
         canDeleteTask = true;
         canEditPrj = true;
@@ -64,34 +104,41 @@ class ProjectManager : User
     };
 };
 
-class Task
-{
-  public:
-    int id;
-    string type;
-    int priority;
-    int size;
-    string assignDate;
-    string dueDate;
-    string completionDate;
-    string title;
-    string assignees;
-    string status; // to-do, doing, done
-    string issues[10];
-};
-
-class Project
-{
-  public:
+class Project {
+public:
     int id;
     string name;
     string description;
     string assignDate;
     string dueDate;
     string completionDate;
-    Task tasks[10];
+    vector <Task> tasks;
     string projectOwnerUserName;
     string projectManagerUserName;
+
+    Json::Value toJson() {
+        Json::Value proj_json;
+        proj_json["id"] = id;
+        proj_json["name"] = name;
+        proj_json["description"] = description;
+        proj_json["assignDate"] = assignDate;
+        proj_json["dueDate"] = dueDate;
+        proj_json["completionDate"] = completionDate;
+
+
+        Json::Value tasks_json;
+        for (Task t: tasks) tasks_json.append(t.toJson());
+        proj_json["tasks"] = tasks_json;
+
+        proj_json["projectOwnerUserName"] = projectOwnerUserName;
+        proj_json["projectManagerUserName"] = projectManagerUserName;
+
+        return proj_json;
+
+
+    }
+
+
 };
 
 // Define user command line state,
